140ff02e8da3410bb7938d59f9d16917a26f9fd2
Nikolay Ivanov <81824679+schukchu@users.noreply.github.com>
2023-01-21 09:53:06 +0700

    Update 12.py

--- второе полугодие/12.py
+++ второе полугодие/12.py
@@ -5,3 +5,13 @@ while cod.count("8888")>=1 or cod.count("1111")>=1:
     else:
         cod = cod.replace("8888", "11", 1)
 print(cod)
+
+from math import*
+for n in range (0,200):
+    col = 4 * n + 117
+    tun = 0
+    for x in range (1, 2000):
+        if col % x  == 0:
+            tun += 1
+    if tun == 2:
+        print(n)
