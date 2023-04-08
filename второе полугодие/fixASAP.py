6f894f032f53cb967d4b5caf32e2df4685c33db2
Nikolay Ivanov <81824679+schukchu@users.noreply.github.com>
2023-04-08 10:12:37 +0700

    Create fixASAP.py

---
+++ второе полугодие/fixASAP.py
@@ -0,0 +1,11 @@
+ki = ['*   * ', '*  ** ', '* * * ', '**  * ']
+mi = ['*   * ', '** ** ', '* * * ', '*   * ']
+
+wor = str(input())
+for x in wor:
+    if wor[x] == 'м':
+        
+    elif wor[x] == 'и':
+    
+    else:
+        print("Dumbass use intended letters")
