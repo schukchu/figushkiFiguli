bf7de3cc407f4368a20cb8ec7f5347d590b56ac6
Nikolay Ivanov <81824679+schukchu@users.noreply.github.com>
2023-05-06 11:35:47 +0700

    Update dop_sort.py

--- второе полугодие/dop_sort.py
+++ второе полугодие/dop_sort.py
@@ -1,2 +1,9 @@
-print(a[i] for i in range(len(a))
-print(c for c in a)
+a = ["4","6","9"]
+print(a[0])
+print(list(c for c in a))
+print(list((a[i] for i in range(len(a)))))
+print(*a,sep='\n')
+for i in a:
+    print(i)
+for i in range(len(a)):
+    print(i)
