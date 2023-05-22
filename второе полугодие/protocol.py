cb7abd2cebc56d6da7ed43cdaebe29840aa09a52
Nikolay Ivanov <81824679+schukchu@users.noreply.github.com>
2023-05-22 14:04:11 +0700

    Update protocol.py

--- второе полугодие/protocol.py
+++ второе полугодие/protocol.py
@@ -1,2 +1,19 @@
 if all(i % y != 0 for y in range (2, math.isqrt(i))):
   - проверка на prime
+import math
+
+til = []
+ter = []
+for i in range(1050, 15000000):
+    tre = str(i)
+    if tre[0:2] == '12' and tre[-4:-5:-1] == '4' and tre[-1:-3:-1] == '56':
+        print(i)
+        if i%161==0:
+            til.append(tre)
+            ker = str(int(ter) / 161)
+            ter = ter.append(ker)
+til = til[::-1]
+for tilt in til:
+    print(tilt)
+for tert in ter:
+    print(tert)
